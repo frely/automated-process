f8f3dd361d3d47149f07d36d5bb3425ed3cfa1ba
frely <frely161@gmail.com>
2024-08-15 09:54:51 +0800

    checkEnv: Add CheckTencent

--- cloud-service/total-expenses/cmd/checkEnv/main.go
+++ cloud-service/total-expenses/cmd/checkEnv/main.go
@@ -5,15 +5,7 @@ import (
 	"os"
 )
 
-func CheckAliyun() {
-	if os.Getenv("ALIBABA_CLOUD_ACCESS_KEY_ID") == "" {
-		fmt.Println("ALIBABA_CLOUD_ACCESS_KEY_ID Not Find")
-		os.Exit(0)
-	}
-	if os.Getenv("ALIBABA_CLOUD_ACCESS_KEY_SECRET") == "" {
-		fmt.Println("ALIBABA_CLOUD_ACCESS_KEY_SECRET Not Find")
-		os.Exit(0)
-	}
+func checkEnv (
 	if os.Getenv("POSTGRES_HOST") == "" {
 		fmt.Println("POSTGRES_HOST Not Find")
 		os.Exit(0)
@@ -32,6 +24,30 @@ func CheckAliyun() {
 	}
 	if os.Getenv("POSTGRES_PASSWORD") == "" {
 		fmt.Println("POSTGRES_PASSWORD Not Find")
+		os.Exit(0)
+	}
+)
+
+func CheckAliyun() {
+	checkEnv()
+	if os.Getenv("ALIBABA_CLOUD_ACCESS_KEY_ID") == "" {
+		fmt.Println("ALIBABA_CLOUD_ACCESS_KEY_ID Not Find")
+		os.Exit(0)
+	}
+	if os.Getenv("ALIBABA_CLOUD_ACCESS_KEY_SECRET") == "" {
+		fmt.Println("ALIBABA_CLOUD_ACCESS_KEY_SECRET Not Find")
+		os.Exit(0)
+	}
+}
+
+func CheckTencent() {
+	checkEnv()
+	if os.Getenv("SecretId") == "" {
+		fmt.Println("SecretId Not Find")
+		os.Exit(0)
+	}
+	if os.Getenv("SecretKey") == "" {
+		fmt.Println("SecretKey Not Find")
 		os.Exit(0)
 	}
 }
