3f2cfb20ec662590dd9eab456d22d9ca3393f8e8
frely <frely161@gmail.com>
2024-08-23 14:39:37 +0800

    ecs: Update

--- cloud-service/ecs/main.go
+++ cloud-service/ecs/main.go
@@ -1,13 +1,8 @@
 package main
 
-import (
-	"github.com/automated-process/ecs/cmd/aliyunDescribeInstances"
-	"github.com/automated-process/ecs/cmd/checkEnv"
-)
-
 func main() {
 	// 阿里云
-	checkEnv.CheckAliyun()
-	aliyunDescribeInstances.CheckSqlTable()
-	aliyunDescribeInstances.Get()
+	//checkEnv.CheckAliyun()
+	//aliyunDescribeInstances.CheckSqlTable()
+	//aliyunDescribeInstances.Get()
 }
