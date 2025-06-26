f1835a46b2a314c37a537079a2fbc4cf840b06d7
frely <frely161@gmail.com>
2025-06-26 14:10:27 +0800

    total-expenses: 初始化配置并添加阿里云和腾讯云每日账单查询的注释

--- cloud-service/total-expenses/main.go
+++ cloud-service/total-expenses/main.go
@@ -1,6 +1,13 @@
 package main
 
+import (
+	"github.com/frely/automated-process/cloud-service/total-expenses/cmd/config"
+)
+
 func main() {
+
+	config.Init()
+
 	// 阿里云
 	// 账号余额查询
 	//fmt.Println(aliyunQueryAccountBalance.Get())
@@ -14,4 +21,10 @@ func main() {
 
 	//账单总览查询
 	//tencentDescribeBillSummaryByProduct.ToSql()
+
+	// 阿里云每日账单查询
+	//aliyunDescribeInstanceBill.ToSql()
+
+	// 腾讯云每日账单查询
+	//tencentDescribeBillDetail.ToSql()
 }
